113b59e6f0c89fa3fdbdc3056017920169c545df
Pedro Murta <pedromurta.7@gmail.com>
2018-07-23 17:30:20 -0300

    criando script da tabela transações

---
+++ scripts SQL/create-table-transacoes.sql
@@ -0,0 +1,26 @@
+﻿-- Table: public.transacoes
+
+-- DROP TABLE public.transacoes;
+
+CREATE TABLE public.transacoes
+(
+  id integer NOT NULL,
+  "Valor" double precision,
+  "siglaUF" character varying,
+  "NBS" character varying,
+  "Ano" integer,
+  "Modo" character varying,
+  "idPais" integer,
+  CONSTRAINT transacoes_pkey PRIMARY KEY (id),
+  CONSTRAINT "transacoes_idPais_fkey" FOREIGN KEY ("idPais")
+      REFERENCES public.pais ("ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "transacoes_siglaUF_fkey" FOREIGN KEY ("siglaUF")
+      REFERENCES public."UF-Br" (sigla) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.transacoes
+  OWNER TO postgres;
