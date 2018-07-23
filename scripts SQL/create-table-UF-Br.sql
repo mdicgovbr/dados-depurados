fb50c4afd202e02ee0555e43293594342b6e412e
Pedro Murta <pedromurta.7@gmail.com>
2018-07-23 17:29:32 -0300

    criando script da tabela UF-Br

---
+++ scripts SQL/create-table-UF-Br.sql
@@ -0,0 +1,15 @@
+﻿-- Table: public."UF-Br"
+
+-- DROP TABLE public."UF-Br";
+
+CREATE TABLE public."UF-Br"
+(
+  sigla character varying NOT NULL,
+  nome character varying,
+  CONSTRAINT "UF-Br_pkey" PRIMARY KEY (sigla)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."UF-Br"
+  OWNER TO postgres;
