8f7662f4a0169f65233a24c7e126e6563f46bcfd
Nitai Bezerra <nitaibezerra@protonmail.com>
2018-08-02 11:36:05 -0300

    Incluindo comentário sobre regras de sigilo

--- scripts SQL/gera-insert-transacoes_aquisicoes.sql
+++ scripts SQL/gera-insert-transacoes_aquisicoes.sql
@@ -7,11 +7,12 @@ Select
 
 FROM(
     select 
-    sum(OP.VALOROPERACAOUSD) as valor, 
-    RAS.UF as uf_origem, 
-    RAS.CODIGOPAISVENDEDOR as codigo_país, 
-    pai.nome as nome_país,
-    OP.CODIGONBS as NBS
+    sum(OP.VALOROPERACAOUSD) as valor
+    ,RAS.UF as uf_origem
+    ,RAS.CODIGOPAISVENDEDOR as codigo_país
+    ,pai.nome as nome_país
+    ,OP.CODIGONBS as NBS
+--    ,count(XXX) as qtd_registros
     
     FROM SISCOSERVA201804.DADOSOPERACOES OP
     JOIN SISCOSERVA201804.DADOSRAS RAS on RAS.ID_RAS = OP.ID_RAS
@@ -21,5 +22,9 @@ FROM(
     WHERE rownum <= 100
     
     GROUP BY RAS.UF, RAS.CODIGOPAISVENDEDOR, OP.CODIGONBS, pai.nome
-);
+)
+
+-- Incluir filtro para remover registros fora das regra de sigilo
+-- WHERE qtd_registros >= 4
+;
 
