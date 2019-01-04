6542623ed9c9494674ecc0feff6cbf76bfec6f53
bugdic100 <bugdic100@gmail.com>
2019-01-04 18:02:03 -0200

    Falta: passo 6 e tratamento do arquivo taxas

--- scripts_depuracao/1 - TrataDados.R
+++ scripts_depuracao/1 - TrataDados.R
@@ -10,41 +10,41 @@
 
 # MELHORIA: recriar codigo de leitura de arquivos com um laco for
 
-  operacoes <- read_delim('2 - dados/operacoes.csv',delim = ';',col_names = TRUE,n_max = 20000)
+  operacoes <- read_delim('dados/operacoes.csv',delim = ';',col_names = TRUE,col_types = cols(.default = 'c'))
 
-  rvs <- read_delim('2 - dados/rvs.csv',delim = ';',col_names = TRUE,n_max = 20000)
+  rvs <- read_delim('dados/rvs.csv',delim = ';',col_names = TRUE,col_types = cols(.default = 'c'))
 
-  item_fat <- read_delim('2 - dados/item_fat.csv',delim = ';',col_names = TRUE,n_max = 20000)
+  item_fat <- read_delim('dados/item_fat.csv',delim = ';',col_names = TRUE,col_types = cols(.default = 'c'))
 
-  fatura <- read_delim('2 - dados/faturas.csv',delim = ';',col_names = TRUE,n_max = 20000)
+  fatura <- read_delim('dados/faturas.csv',delim = ';',col_names = TRUE,col_types = cols(.default = 'c'))
 
-  vendedores <- read_delim('2 - dados/vendedores.csv',delim = ';',col_names = TRUE)
+  vendedores <- read_delim('dados/vendedores.csv',delim = ';',col_names = TRUE,col_types = cols(.default = 'c'))
 
-  serv_enq_vd <- read_delim('2 - dados/serv_enq_vd.csv',delim = ';',col_names = TRUE)
+  serv_enq_vd <- read_delim('dados/serv_enq_vd.csv',delim = ';',col_names = TRUE,col_types = cols(.default = 'c'))
 
-  taxa_venda <- read_delim('2 - dados/taxa_venda.csv',delim = ';',col_names = TRUE)
+  taxa_venda <- read_delim('dados/taxa_venda.csv',delim = ';',col_names = TRUE,col_types = cols(.default = 'c'))
 
 # tabelas apoio
   
-  paises <- as.data.frame(read_delim('2 - dados/paises.csv',delim = ',',col_names = TRUE))
+  paises <- as.data.frame(read_delim('dados/paises.csv',delim = ',',col_names = TRUE),col_types = cols(.default = 'c'))
   
-    paises$CODIGO <- gsub("(?<![0-9])0+", "", paises$CODIGO, perl = TRUE)
+    paises$CODIGO <- gsub("(?<![0-9])0+", "", paises$CODIGO, perl = TRUE,col_types = cols(.default = 'c'))
   
-  moeda <- as.data.frame(read_delim('2 - dados/moedas.csv',delim = ',',col_names = TRUE,locale = locale(encoding = "latin1"),quote = ''))
+  moeda <- as.data.frame(read_delim('dados/moedas.csv',delim = ',',col_names = TRUE,locale = locale(encoding = "latin1"),quote = '',col_types = cols(.default = 'c')))
   
     moeda$CODIGO <- gsub('"','',moeda$CODIGO)
     
     moeda$CODIGO <- gsub("(?<![0-9])0+", "", moeda$CODIGO, perl = TRUE)
 
-  nbs <- as.data.frame(read_delim('2 - dados/nbs.csv',delim = ',',col_names = TRUE))
+  nbs <- as.data.frame(read_delim('dados/nbs.csv',delim = ',',col_names = TRUE),col_types = cols(.default = 'c'))
   
-  enquad <- read_delim('2 - dados/enquadramento.csv',delim = ',',col_names = TRUE)
+  enquad <- read_delim('dados/enquadramento.csv',delim = ',',col_names = TRUE,col_types = cols(.default = 'c'))
   
-  mapa <- read_delim('2 - dados/mapeamento.csv',delim = ',',col_names = TRUE)
+  mapa <- read_delim('dados/mapeamento.csv',delim = ',',col_names = TRUE,col_types = cols(.default = 'c'))
   
-  matriz <- read_delim('2 - dados/matriz.csv',delim = ',',col_names = TRUE)
+  matriz <- read_delim('dados/matriz.csv',delim = ',',col_names = TRUE,col_types = cols(.default = 'c'))
   
-  meses <- read_delim('2 - dados/meses.csv',delim = ',',col_names = TRUE)
+  meses <- read_delim('dados/meses.csv',delim = ',',col_names = TRUE,col_types = cols(.default = 'c'))
 
 ##################### ############################ ##########################################
 
@@ -62,7 +62,7 @@
   
   taxa_venda <- taxa_venda %>%
     group_by(DATA_TAXA,MOEDA) %>% 
-    summarise(TAXA = first(taxa_venda$TAXA,taxa_venda$DATA_TAXA,taxa_venda$MOEDA))
+    summarise(TAXA = first(taxa_venda$TAXA))
   
 ##################### ################################ ######################################
 
@@ -89,15 +89,8 @@
                        
   )
   
-  rvs$ID_VENDEDOR <- ifelse(str_count(rvs$ID_VENDEDOR)==11,
-                    str_pad(string = rvs$ID_VENDEDOR,width = 14,side = 'right',pad = ' '),rvs$ID_VENDEDOR)
-  
-  vendedores$CPF_CNPJ_DESF <- ifelse(str_count(vendedores$CPF_CNPJ_DESF)==11,
-                            str_pad(string = vendedores$CPF_CNPJ_DESF,width = 14,side = 'right',pad = ' '),vendedores$CPF_CNPJ_DESF)
-  
-  
   rvs <-  rvs %>%
-          mutate(VENDEDOR_INEXISTENTE = if_else((rvs$ID_VENDEDOR %in% vendedores$CPF_CNPJ_DESF),0,1))
+          mutate(VENDEDOR_INEXISTENTE = if_else((rvs$ID_VENDEDOR %in% str_trim(vendedores$CPF_CNPJ_DESF)),0,1))
   
 # Passo 3 - Marca moedas inexistentes
 
@@ -110,59 +103,42 @@
     mutate(PAIS_INEXISTENTE = if_else((rvs$PAIS_ADQUIRENTE %in% paises$CODIGO),0,1))
 
 # Passo 5 - Marca rvs inexistentes - Operacoes
-   
+  
    operacoes <- operacoes %>% 
      
      mutate(RVS_INEXISTENTE = 
    
-      if_else( 0 ==  
-                 ifelse(rvs$ID_RVS %in% operacoes$ID_RVS ,
-        
-                          count(rvs %>% 
-                                  
-                                  select(MOEDA_INEXISTENTE,PAIS_INEXISTENTE,VENDEDOR_INEXISTENTE) %>% 
-                                  
-                                  filter(NVL(rvs$MOEDA_INEXISTENTE,0) == 0 &
+      if_else( 0 ==  count(rvs %>% 
+                             
+                             select(MOEDA_INEXISTENTE,PAIS_INEXISTENTE,VENDEDOR_INEXISTENTE) %>% 
+                             
+                             filter(NVL(rvs$MOEDA_INEXISTENTE,0) == 0 &
                                          NVL(rvs$PAIS_INEXISTENTE,0)  == 0 &
-                                         NVL(rvs$VENDEDOR_INEXISTENTE,0) == 0
+                                         NVL(rvs$VENDEDOR_INEXISTENTE,0) == 0 &
+                                          rvs$ID_RVS %in% operacoes$ID_RVS   
                                         )
                                 )
-                 ,0)
-               
                ,1,0)
       )
    
 # Passo 6 - Copia, atualiza e marca NBS inexistentes NBS
-
+   
+  # ALTER TABLE IMPORTACAOOPERACOES ADD NBS_ATUALIZADA VARCHAR2(255);
+  # UPDATE IMPORTACAOOPERACOES SET NBS_ATUALIZADA = NBS;
+   
+   operacoes <- operacoes %>% mutate(NBS_ATUALIZADA = NBS)
+   
   # UPDATE /*+ parallel(OPER,8) */ SISCOSERVV.IMPORTACAOOPERACOES OPER
   # SET OPER.NBS_ATUALIZADA = ( SELECT MAP.CODIGOCLASSIFICACAO_V1_1
   #                             FROM SISCOSERVV.MAPEAMENTONBSV1_0_V1_1 MAP
   #                             WHERE OPER.NBS_ATUALIZADA = MAP.CODIGOCLASSIFICACAO_V1_0)
   #                             WHERE OPER.NBS_ATUALIZADA NOT IN (select NBSV1_1_07CODIGOSCLASSIFICACAO.CODIGO from SISCOSERVV.NBSV1_1_07CODIGOSCLASSIFICACAO);
    
-   operacoes <- operacoes %>% 
-     
-     mutate(NBS_ATUALIZADA = 
    
-      mapa %>% 
-           
-           select(CODIGOCLASSIFICACAO_V1_1) %>% 
-           
-           filter(operacoes$NBS_ATUALIZADA %in% mapa$CODIGOCLASSIFICACAO_V1_0 &
-                  operacoes$NBS_ATUALIZADA !(nbs$CODIGO)
-                  
-                  )
-     )
-    
-  # UPDATE /*+ parallel(OPER,8) */ siscoservv.IMPORTACAOOPERACOES Oper
-  # SET NBS_INEXISTENTE = 1
-  # WHERE not exists (SELECT 1
-  #                   FROM siscoservv.NBSV1_1_07CODIGOSCLASSIFICACAO N
-  #                   WHERE N.CODIGO =  Oper.NBS_ATUALIZADA);
    
-   operacoes <- operacoes %>% 
-     mutate(NBS_INEXISTENTE = if_else(0 == count(if_else(nbs$CODIGO %in% operacoes$NBS_ATUALIZADA,1,0)),1,0))
-
+  operacoes <- operacoes %>% 
+    mutate(NBS_INEXISTENTE = if_else(0 == count(nbs %>% filter(nbs$CODIGO %in% operacoes$NBS_ATUALIZADA)),1,0))
+   
 # Passo 7 - Marca paises inexistentes
   
   operacoes <-  operacoes %>% mutate(PAIS_INEXISTENTE = ifelse((operacoes$CD_PAIS_DESTINO %in% paises$CODIGO),0,1))
@@ -191,88 +167,71 @@
 
 # Passo 9 - Marca operacoes inexistentes
 
-  # UPDATE /*+ parallel(ENQ,8) */ siscoservv.IMPORTACAOENQUADRAMENTOS ENQ
-  # SET Enq.OPERACAO_INEXISTENTE = 1
-  # WHERE not exists(SELECT 1
-  #                  FROM siscoservv.IMPORTACAOOPERACOES Oper
-  #                  WHERE nvl(Oper.RVS_INEXISTENTE,0) = 0 -- FALSE
-  #                  AND nvl(Oper.NBS_INEXISTENTE,0) = 0 --FALSE
-  #                  AND nvl(Oper.PAIS_INEXISTENTE,0) = 0 --FALSE
-  #                  AND Enq.ID_OPERACAO = Oper.ID_OPERACAO);
+  serv_enq_vd <- serv_enq_vd %>% 
+    
+    mutate(OPERACAO_INEXISTENTE = if_else(0 == count(operacoes %>% 
+                                                       select(RVS_INEXISTENTE,NBS_INEXISTENTE,PAIS_INEXISTENTE) %>% 
+                                                       filter(NVL(operacoes$RVS_INEXISTENTE,0) == 0 &
+                                                                NVL(operacoes$NBS_INEXISTENTE,0)  == 0 &
+                                                                NVL(operacoes$PAIS_INEXISTENTE,0) == 0 &
+                                                                serv_enq_vd$ID_SERVICO %in% operacoes$ID_OPERACAO
+                                                              )
+                                                     )
+                                          ,1,0)
+           )
   
-             
-                               
-                               count(operacoes %>% 
-                                       
-                                       select(RVS_INEXISTENTE,NBS_INEXISTENTE,PAIS_INEXISTENTE) %>% 
-                                       
-                                       filter(NVL(operacoes$RVS_INEXISTENTE,0) == 0 &
-                                              NVL(operacoes$PAIS_INEXISTENTE,0)  == 0 &
-                                              NVL(operacoes$NBS_INEXISTENTE,0) == 0))
-
-  
-  
-
 # Passo 10 - Marca enquadramento inexistentes 
                                
-         serv_enq_vd <- serv_enq_vd %>% mutate(ENQUAD_INEXISTENTE = ifelse(serv_enq_vd$CD_ENQUADRAMENTO %in% enquad$CODIGO,0,1))
+   serv_enq_vd <- serv_enq_vd %>%
+     mutate(ENQUAD_INEXISTENTE = ifelse(serv_enq_vd$CD_ENQUADRAMENTO %in% enquad$CODIGO,0,1)
+            )
            
 # Passo 11 - Marca RVS inexistente
-
-  # UPDATE /*+ parallel(RF,4) */ siscoservv.IMPORTACAORF RF
-  # SET RVS_INEXISTENTE = 1
-  # WHERE not exists (SELECT 1
-  #                   FROM  siscoservv.IMPORTACAORVS RVS
-  #                   WHERE nvl(RVS.MOEDA_INEXISTENTE,0) = 0
-  #                   AND nvl(RVS.PAIS_INEXISTENTE,0) = 0
-  #                   AND nvl(RVS.VENDEDOR_INEXISTENTE,0) = 0
-  #                   AND RVS.ID_RVS = RF.ID_RVS);
          
+  fatura <- fatura %>%
+    mutate(RVS_INEXISTENTE = 
+             if_else(0 == count(rvs %>% 
+                                  select(MOEDA_INEXISTENTE,PAIS_INEXISTENTE,VENDEDOR_INEXISTENTE) %>% 
+                                  filter(NVL(rvs$MOEDA_INEXISTENTE,0) == 0 &
+                                           NVL(rvs$PAIS_INEXISTENTE,0)  == 0 &
+                                           NVL(rvs$VENDEDOR_INEXISTENTE,0) == 0 &
+                                           rvs$ID_RVS %in% fatura$ID_RVS   
+                                         )
+                                )
+                     ,1,0)
+           )
          
-         count(rvs %>% 
-                 
-                 select(MOEDA_INEXISTENTE,VENDEDOR_INEXISTENTE,PAIS_INEXISTENTE) %>% 
-                 
-                 filter(NVL(operacoes$MOEDA_INEXISTENTE,0) == 0 &
-                        NVL(operacoes$PAIS_INEXISTENTE,0)  == 0 &
-                        NVL(operacoes$VENDEDOR_INEXISTENTE,0) == 0))
-
 # Passo 12 - Marca RF inexistente
 
-  # UPDATE /*+ parallel(PGTO,8) */ siscoservv.IMPORTACAOITENSFATURAMENTO FAT
-  # SET RF_INEXISTENTE = 1
-  # WHERE not exists (SELECT 1
-  #                   FROM siscoservv.IMPORTACAORF RF
-  #                   WHERE nvl(RF.RVS_INEXISTENTE,0) = 0
-  #                   AND RF.ID_RF = FAT.ID_RF);
-         
-         count(fatura %>% 
-                 
-                 select(RVS_INEXISTENTE) %>% 
-                 
-                 filter(NVL(fatura$RVS_INEXISTENTE,0) == 0))
-         
-         
+  item_fat <- item_fat %>% 
+    mutate(FATURA_INEXISTENTE = 
+             if_else(0 == count(fatura %>% 
+                                  select(RVS_INEXISTENTE) %>%
+                                  filter(NVL(fatura$RVS_INEXISTENTE,0) == 0 &
+                                           fatura$ID_FATURA %in% item_fat$ID_FATURA)
+                     )
+          ,1,0)
+    )
   
 # Passo 13 - Marca operacoes inexistentes
 
-  # UPDATE /*+ parallel(PGTO,8) */ siscoservv.IMPORTACAOITENSFATURAMENTO FAT
-  # SET OPERACAO_INEXISTENTE = 1
-  # WHERE not exists(SELECT 1
-  #                  FROM  siscoservv.IMPORTACAOOPERACOES Oper
-  #                   WHERE nvl(Oper.RVS_INEXISTENTE,0) = 0
-  #                  AND nvl(Oper.NBS_INEXISTENTE,0) = 0
-  #                  AND nvl(Oper.PAIS_INEXISTENTE,0) = 0
-  #                  AND FAT.ID_OPERACAO = Oper.ID_OPERACAO);
-         
-         count(rvs %>% 
-                 
-                 select(RVS_INEXISTENTE,NBS_INEXISTENTE,PAIS_INEXISTENTE) %>% 
-                 
-                 filter(NVL(operacoes$RVS_INEXISTENTE,0) == 0 &
-                        NVL(operacoes$PAIS_INEXISTENTE,0)  == 0 &
-                        NVL(operacoes$NBS_INEXISTENTE,0) == 0))
-
+  item_fat <- item_fat %>% 
+    
+    mutate(OPERACAO_INEXISTENTE = 
+             if_else(0 == count(operacoes %>% 
+                                  select(RVS_INEXISTENTE,NBS_INEXISTENTE,PAIS_INEXISTENTE) %>% 
+                                  filter(
+                                    NVL(operacoes$RVS_INEXISTENTE,0) == 0 &
+                                    NVL(operacoes$PAIS_INEXISTENTE,0)  == 0 &
+                                    NVL(operacoes$NBS_INEXISTENTE,0) == 0 &
+                                    fatura$ID_FATURA %in% operacoes$ID_OPERACAO 
+                                    )
+                                )
+                     ,1,0)
+           )
+    
+  
+  
 # Passo 14 - Calcula Valor Pago em USD - Itens de FATURAMENTO
 
   # UPDATE /*+ parallel(PGTO,8) */ siscoservv.IMPORTACAOITENSFATURAMENTO FAT
@@ -343,17 +302,6 @@
   # AND nvl(VENDEDOR_INEXISTENTE,0) = 0
   # AND MOEDA  in (select CODIGO from siscoservv.moeda) order by 1 ;
          
-         count(rvs %>% 
-                 
-                 select(all(rvs)) %>% 
-                 
-                 filter(NVL(rvs$MOEDA_INEXISTENTE,0) == 0 &
-                          NVL(rvs$PAIS_INEXISTENTE,0)  == 0 &
-                          NVL(rvs$VENDEDOR_INEXISTENTE,0) == 0 &
-                          MOEDA 
-                 )
-         )
-  
 # c) Copia Dados DE Importacao - Operacoes PARA Operacoes
 
   # --ALTER TABLE siscoservv.DADOSOPERACOES INITRANS 10;
